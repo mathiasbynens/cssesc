7dfd4b53f859bb2337f827ecc83c1e7e5d3596a9
Mathias Bynens <mathias@qiwi.be>
2013-08-09 15:28:41 +0200

    Add cssesc.js

---
+++ src/data.js
@@ -0,0 +1,15 @@
+var regenerate = require('regenerate');
+var fs = require('fs');
+
+// Characters with special meaning in CSS, except for quotes and backslashes
+// (they get a separate regex)
+var set = regenerate().add(
+	' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ';', '<',
+	'=', '>', '?', '@', '[', ']', '^', '`', '{', '|', '}', '~', '"', '\'', '\\'
+);
+
+module.exports = {
+	'anySingleEscape': set.toString(),
+	'singleEscapes': set.remove('"', '\'', '\\').toString(),
+	'version': JSON.parse(fs.readFileSync('package.json', 'utf8')).version
+};
