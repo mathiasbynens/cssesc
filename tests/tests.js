9df37c957b6db559c6ed860d3525ffaa60505d2a
Mathias Bynens <mathias@qiwi.be>
2015-03-12 13:55:19 +0100

    Update dependencies

--- tests/tests.js
+++ tests/tests.js
@@ -1,22 +1,25 @@
-(function(root) {
-	'use strict';
+(function() {
+	// Used as reference to the global object.
+	var root = (typeof global == 'object' && global) || this;
 
+	// Used as a no-op function.
 	var noop = Function.prototype;
 
-	var load = (typeof require == 'function' && !(root.define && define.amd)) ?
-		require :
-		(!root.document && root.java && root.load) || noop;
+	// Use a single `load` function.
+	var load = (typeof require == 'function' && !(root.define && define.amd))
+		? require
+		: (!root.document && root.java && root.load) || noop;
 
-	var QUnit = (function() {
-		return root.QUnit || (
-			root.addEventListener || (root.addEventListener = noop),
-			root.setTimeout || (root.setTimeout = noop),
-			root.QUnit = load('../node_modules/qunitjs/qunit/qunit.js') || root.QUnit,
-			addEventListener === noop && delete root.addEventListener,
-			root.QUnit
-		);
-	}());
+	// Load QUnit in a way that works around cross-environment issues.
+	var QUnit = root.QUnit || (root.QUnit = (
+		root.addEventListener || (root.addEventListener = noop),
+		root.setTimeout || (root.setTimeout = noop),
+		QUnit = load('../node_modules/qunitjs/qunit/qunit.js') || root.QUnit,
+		addEventListener === noop && delete root.addEventListener,
+		QUnit = QUnit.QUnit || QUnit
+	));
 
+	// Load QUnit Extras.
 	var qe = load('../node_modules/qunit-extras/qunit-extras.js');
 	if (qe) {
 		qe.runInContext(root);
@@ -29,163 +32,163 @@
 
 	/*--------------------------------------------------------------------------*/
 
-	test('cssesc: common usage', function() {
-		equal(
+	QUnit.test('cssesc: common usage', function() {
+		QUnit.equal(
 			typeof cssesc.version,
 			'string',
 			'`cssesc.version` must be a string'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-foo'),
 			'-foo',
 			'-foo'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('--foo', { 'isIdentifier': false }),
 			'--foo',
 			'--foo with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('--foo', { 'isIdentifier': true }),
 			'\\--foo',
 			'--foo with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-0foo', { 'isIdentifier': false }),
 			'-0foo',
 			'-0foo with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-0foo', { 'isIdentifier': true }),
 			'\\-0foo',
 			'-0foo with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-9foo', { 'isIdentifier': false }),
 			'-9foo',
 			'-9foo with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-9foo', { 'isIdentifier': true }),
 			'\\-9foo',
 			'-9foo with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('foo:bar', { 'isIdentifier': false }),
 			'foo:bar',
 			'foo:bar (avoid `\\:` for IE < 8 compatibility) with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('foo:bar', { 'isIdentifier': true }),
 			'foo\\3A bar',
 			'foo:bar (avoid `\\:` for IE < 8 compatibility) with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('_foo_bar', { 'isIdentifier': false }),
 			'_foo_bar',
 			'_foo_bar with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('_foo_bar', { 'isIdentifier': true }),
 			'\\_foo_bar',
 			'_foo_bar (escape leading `_` for IE6 compatibility) with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a\t\n\v\f\rb'),
 			'a\\9\\A\\B\\C\\D b',
 			'whitespace characters'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('\\A _'),
 			'\\\\A _',
 			'backslash escapes that look like a hex escape: space is preserved'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('\\\\A _'),
 			'\\\\\\\\A _',
 			'backslash escapes that look like a hex escape: space is preserved'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a\\b'),
 			'a\\\\b',
 			'backslash'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-\\ABC -', { 'isIdentifier': false }),
 			'-\\\\ABC -',
 			'more backslashes with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('-\\ABC -', { 'isIdentifier': true }),
 			'-\\\\ABC\\ -',
 			'more backslashes with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a"b\'c\xA9d', { 'wrap': true }),
 			'\'a"b\\\'c\\A9 d\'',
 			'quotes with `wrap: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a"b\'c\xA9d', { 'wrap': true, 'quotes': 'LOLWAT' }),
 			'\'a"b\\\'c\\A9 d\'',
 			'quotes with `wrap: true, quotes: \'LOLWAT\'` (incorrect value)'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a"b\'c\xA9d', { 'wrap': true, 'quotes': 'single' }),
 			'\'a"b\\\'c\\A9 d\'',
 			'quotes with `wrap: true, quotes: \'single\'`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a"b\'c\xA9d', { 'wrap': true, 'quotes': 'double' }),
 			'"a\\"b\'c\\A9 d"',
 			'quotes with `wrap: true, quotes: \'double\'`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a\xA9b'),
 			'a\\A9 b',
 			'non-ASCII symbol'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('Ich \u2665 B\xFCcher'),
 			'Ich \\2665  B\\FC cher',
 			'non-ASCII symbols'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a123b'),
 			'a123b',
 			'numbers not at the start of the string'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('123a2b', { 'isIdentifier': false }),
 			'123a2b',
 			'numbers at the start of the string with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('123a2b', { 'isIdentifier': true }),
 			'\\31 23a2b',
 			'numbers at the start of the string with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('1_23a2b', { 'isIdentifier': false }),
 			'1_23a2b',
 			'numbers at the start of the string with `isIdentifier: false`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('1_23a2b', { 'isIdentifier': true }),
 			'\\31_23a2b',
 			'numbers at the start of the string with `isIdentifier: true`'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a\uD834\uDF06b'),
 			'a\\1D306 b',
 			'astral symbol'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('a\uD834b'),
 			'a\\D834 b',
 			'lone high surrogate'
 		);
-		equal(
+		QUnit.equal(
 			cssesc('lolwat"foo\'bar\xA9k', {
 				'escapeEverything': true
 			}),
@@ -201,4 +204,4 @@
 	if (!root.document || root.phantom) {
 		QUnit.load();
 	}
-}(typeof global == 'object' && global || this));
+}.call(this));
